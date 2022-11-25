ac5059c7428300136e125104edd6a05b3c18382c
Matthew Huebert <mhuebert@gmail.com>
2022-11-25 10:06:27 +0100

    bring back find-or-create-el

--- src/main/yawn/root.cljs
+++ src/main/yawn/root.cljs
@@ -1,9 +1,19 @@
 (ns yawn.root
-  (:require ["react-dom/client" :as react:dom]))
+  (:require ["react-dom/client" :as react:dom]
+            [applied-science.js-interop :as j]))
+
+(defn find-or-create-element [id]
+  (if (or (string? id) (keyword? id))
+    (or
+     (js/document.getElementById (name id))
+     (-> (js/document.createElement "div")
+         (j/!set :id (name id))
+         (doto (->> js/document.body.append))))
+    id))
 
 (defn render [^js root react-el] (.render root react-el))
 (defn create
-  ([el] (react:dom/createRoot el))
+  ([el] (react:dom/createRoot (find-or-create-element el)))
   ([el content]
    (doto (create el)
      (render content))))
